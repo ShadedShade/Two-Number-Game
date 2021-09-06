9b5902c46059e94a066164788e7867d9dd21b197
ronmattss <31378489+ronmattss@users.noreply.github.com>
2021-09-06 21:53:09 +0800

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,7 +4,7 @@ import LoginTest from './LoginTest.js'
 import Test from './TestButton.js'
 import Signup from './Signup'
 import Login from './Login.js';
-import Nav from './Nav';
+
 
 
 function Home() {
