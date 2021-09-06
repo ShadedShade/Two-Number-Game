44f6663ec5b0d08c38e0f1fde750006d42e8a363
ronmattss <31378489+ronmattss@users.noreply.github.com>
2021-09-06 12:35:10 +0800

    t

--- frontend/src/components/LoginStepTwo.js
+++ frontend/src/components/LoginStepTwo.js
@@ -4,18 +4,15 @@ import Axios from 'axios';
 function LoginStepTwo() {
     const [password, setPassword] = useState('');
     const history = useHistory();
-    const handleBackClick = () =>
-        {
-            history.push('/');
-        }
-        const handleGoToSignUpClick = () =>
-        {
-            history.push('/SignUp');
-        }
-        const handleLoginStepTwoClick = () =>
-        {
-            history.push('/logintwo'); // go to step 2
-        }
+    const handleBackClick = () => {
+        history.push('/');
+    }
+    const handleGoToSignUpClick = () => {
+        history.push('/SignUp');
+    }
+    const handleLoginStepTwoClick = () => {
+        history.push('/logintwo'); // go to step 2
+    }
 
     const Login = () => {
         Axios.post('http://localhost:3000/login', {
@@ -28,52 +25,58 @@ function LoginStepTwo() {
                 setLoginStatus(response.data[0])
                 console.log(response);
                 console.log(response.data[0].mobile);
-                sessionStorage.setItem("Userid",response.data[0].mobile);
-               console.log("session Storage Userid: " +  sessionStorage.getItem("Userid"));
+                sessionStorage.setItem("Userid", response.data[0].mobile);
+                console.log("session Storage Userid: " + sessionStorage.getItem("Userid"));
 
             }
         });
     }
     return (
-        <section class="container">
-            <a href="" onClick={handleBackClick}>
-                <span><i class="fas fa-arrow-left back"> Back</i></span>
-            </a>
-            <div class="row d-flex justify-content-center align-items-center bg-row">
-                <div class="col-md-5 bg-box p-5">
-                    <h3 class="text-center">Login</h3>
-                    <form>
-                        <div class="mb-4 pb-2 mt-5">
-                            <label for="userid">Enter User ID / Mobile Number</label>
-                            <input type="text" name="userid" class="form-control" id="userid" onChange={(e) => { setUsername(e.target.value) }}/>
-                        </div>
-                        <div class="mt-4 mb-3 d-grid gap-2">
-                            {/* GO TO STEP TWO */}
-                            <a class="btn" onClick={Login}>NEXT</a>
-                        </div>
-                        <div class="text-center forgot">
-                            {/* AHH WAT TO DO HERE? */}
-                            <p class="mb-0">Forgot UserID / Mobile Number? Call our <br /></p>
-                            <a class="CCC" href="">Customer Contact Center</a>
-                        </div>
-                        <div class="col-md-12 mt-4">
-                            <div class="login-or">
-                                <hr class="hr-or" />
-                                <span class="span-or">or login in with</span>
-                            </div>
-                        </div>
-                        <div class="col-md-12 mt-4 text-center">
-                            <span><i class="fab fa-facebook-square px-2"></i></span>
-                            <span><i class="fab fa-google-plus-square px-2"></i></span>
-                        </div>
-                        <div class="text-center mt-3 signup">
-                            <p class="mb-0">Don't have an account yet? <a class="CCC" onClick={handleGoToSignUpClick}>Sign Up</a></p>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </section>
-    );
-}
 
+        <section>
+
+            <section class="container">
+                    <div class="row d-flex justify-content-center align-items-center bg-row">
+                        <div class="col-md-5 bg-box p-5">
+                            <a href="login.html">
+                                <span><i class="fas fa-arrow-left"></i></span>
+                        </a>
+                        <h3 class="text-center">Login</h3>
+                        <div class="mb-0 mt-5">
+                            <label for="userid">Enter your 6-digit MPIN</label>
+                        </div>
+                        <div class="mb-1 mpin">
+                            <input type="text" id='ist' maxlength="1" onkeyup="clickEvent(this, 'sec')"/>
+                                <input type="text" id="sec" maxlength="1" onkeyup="clickEvent(this, 'third')"/>
+                                    <input type="text" id="third" maxlength="1" onkeyup="clickEvent(this, 'fourth')"/>
+                                        <input type="text" id="fourth" maxlength="1" onkeyup="clickEvent(this, 'fifth')"/>
+                                            <input type="text" id="fifth" maxlength="1" onkeyup="clickEvent(this, 'sixth')"/>
+                                                <input type="text" id="sixth" maxlength="1"/>
+                                                </div>
+                                                <div class="text-center forgot">
+                                                    <p class="">Forgot MPIN? Call our <a class="CCC" href="">Customer Contact Center</a></p>
+                                                </div>
+                                                <div class="mt-5 pt-4 d-grid gap-2">
+                                                    <button class="btn">Login</button>
+                                                </div>
+                                                <div class="text-center mt-3 signup">
+                                                    <p class="mb-0">Don't have an account yet? <a class="CCC" href="signup.html">Sign Up</a></p>
+                                                </div>
+                                        </div>
+                                    </div>
+                            </section>
+                        </section>
+
+);
+
+// //        {/* <!-- MPIN --> */}
+//   //      {/* <script type="text/javascript">
+//             function clickEvent(first, last){
+//                 if(first.value.length){
+//                     document.getElementById(last).focus();
+//                 }
+//             }
+//         </script> */}
+
+}
 export default Login
