af690ec95d3fe0573b637e17b7ea4a0835b7e60a
kithhhhh <klbarrientos@iskolarngbayan.pup.edu.ph>
2021-09-09 14:48:03 +0800

    Added Load Wallet Modal in "HomepageContent"

--- frontend/src/components/HomepageContent.js
+++ frontend/src/components/HomepageContent.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
+import DateTimePicker from 'react-datetime-picker';
 import '../styles/hpcontent.css'
 
 
@@ -28,7 +29,7 @@ function HomepageContent() {
                             <h1>
                                 <sup>PTS</sup> 
                                 1,001,115.00
-                                <a href="#"><span><i class="fas fa-plus-circle"></i></span></a>
+                                <button class="loadwallet" data-bs-toggle="modal" data-bs-target="#load"><span><i class="fas fa-plus-circle"></i></span></button>
                             </h1>
                         </div>
                     </div>
@@ -53,9 +54,77 @@ function HomepageContent() {
                                 <label class="description" id="stat">Stats</label>
                             </div>
                         </div>
-                        
-                        
                     </div>
+                </div>
+                {/* <!-- Modal --> */}
+                <div class="modal fade" id="load" tabindex="-1" aria-labelledby="loadModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-fullscreen">
+                    <div class="modal-content">
+                    <div class="modal-header loadModalHeader">
+                        <h5 class="modal-title" id="loadModalLabel">Load <span style={{color: "#f36e23"}}>Wallet</span></h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body p-5">
+                        <form class="pt-5">
+                            <div className="form-content">
+                                <div className="row pb-4">
+                                    <div className="col-md-6">
+                                        <div className="form-group pb-4">
+                                            <label for="senderName">Sender Name</label>
+                                            <input type="text" id="senderName" className="form-control" />
+                                        </div>
+                                        <div className="form-group pb-4">
+                                            <label for="agentID">Select Agent ID</label>
+                                            {/* SELECT MENU HERE */}
+                                            <select class="form-select" id="agentID" aria-label="select">
+                                                <option selected></option>
+                                                <option value="1">First</option>
+                                                <option value="2">Second</option>
+                                                <option value="3">Third</option>
+                                            </select>
+                                            {/* ======================= */}
+                                        </div>
+                                        <div className="form-group pb-4">
+                                            <label for="refnum">Reference Number</label>
+                                            <input type="text" id="refnum" className="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-group pb-4">
+                                            <label for="gcashnum">Gcash Number</label>
+                                            <input type="tel" id="gcashnum" className="form-control" aria-describedby="basic-addon2" minlength="11" maxlength="11" placeholder="09XXXXXXXXX" pattern="[0-9]{2}[0-9]{9}"/>
+                                        </div>
+                                        <div className="form-group pb-4">
+                                            <label for="dtpicker">Select Deposit and Time</label>
+                                            {/* npm install react-datetime-picker or yarn add react-datetime-picker */}
+                                                <div>
+                                                    <DateTimePicker 
+                                                        className="form-control"
+                                                        id="dtpicker"
+                                                        // onChange={onChange}
+                                                        // value={value}
+                                                    />
+                                                </div>
+                                                
+                                            {/* idk kung paano gagawin sorry https://www.npmjs.com/package/react-datetime-picker======================= */}
+                                        </div>
+                                        <div className="form-group pb-4">
+                                            <label for="amount">Amount</label>
+                                            <input type="number" id="amount" className="form-control"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-12 text-center">
+                                    <button type="button" className="btn loadWalletBtn">LOAD WALLET</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer loadModalFooter">
+                        <button type="button" class="btn btn-secondary loadModalCloseBtn" data-bs-dismiss="modal">Close</button>
+                    </div>
+                    </div>
+                </div>
                 </div>
         </div>
     );
